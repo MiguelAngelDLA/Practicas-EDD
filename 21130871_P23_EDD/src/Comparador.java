6e895b3dc61ff2ccf5e7f6f7c0b059a8bb73815e
Miguel Angel de León <49102547+MiguelAngelDLA@users.noreply.github.com>
2022-10-23 22:57:38 -0500

    terminado proyecto de tema 2 y 3

---
+++ 21130871_P23_EDD/src/Comparador.java
@@ -0,0 +1,23 @@
+
+import java.util.Comparator;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author migue
+ */
+public class Comparador
+        implements Comparator<Character> {
+ 
+        @Override
+        public int compare(Character customer1,
+                           Character customer2)
+        {
+            return 0;
+        }
+    }
